8ee01766fec7fe7e5f103a35c4a9286c434f2c0d
Gwynn <rgwynn3@gatech.edu>
2017-11-29 21:20:39 -0500

    loaded scripts for use

--- main/scenes.py
+++ main/scenes.py
@@ -75,13 +75,40 @@ def load_script(filepath):
         scene = re.sub('CONT.+\s', ' ', scene)
         scene = re.sub('OMIT.+\s', ' ', scene)
         scene = re.sub("\s\s+", " ", scene)  # remove double spaces
-        print(scene)
+        #print(scene)
         conn.execute("""insert into Scenes (film_id, data) values (?, ?)""", (film_id, scene))
 
         count += 1
-        print("count: " + str(count))
+        #print("count: " + str(count))
 
 
 if __name__ == '__main__':
     init_db()
-    load_script("8MM.txt")
+    try:
+        #grossing
+        load_script("Avatar.txt")
+        load_script("Dark-Knight-Rises,-The.txt")
+        load_script("Spider-Man.txt")
+        load_script("Pirates-of-the-Caribbean.txt")
+        load_script("Frozen.txt")
+        load_script("Star-Wars-Revenge-of-the-Si.txt")
+        load_script("Star-Wars-The-Force-Awakens.txt")
+        load_script("Lord-of-the-Rings-Return-of-the-King.txt")
+        load_script("Mission-Impossible.txt")
+        load_script("Shrek-the-Third.txt")
+        #rated
+        load_script("Boyhood.txt")
+        load_script("Lost-in-Translation.txt")
+        load_script("12-Years-a-Slave.txt")
+        load_script("Social-Network,-The.txt")
+        load_script("Boyhood.txt")
+        load_script("Zero-Dark-Thirty.txt")
+        load_script("Wall-E.txt")
+        load_script("Sideways.txt")
+        load_script("Amour.txt")
+        load_script("Crouching-Tiger,-Hidden-Dragon.txt")
+        #bad
+        load_script("Hudson-Hawk.txt")
+        load_script("Catwoman.txt")
+    except:
+        print(error)
